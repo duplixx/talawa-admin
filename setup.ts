ddcea2d595cd0993fd49980c678c841c4cdc3634
Ratan Deep Singh <146458278+ratankods@users.noreply.github.com>
2024-02-03 00:32:22 -0800

    Update setup.ts

--- setup.ts
+++ setup.ts
@@ -10,10 +10,10 @@ async function checkConnection(url: string): Promise<any> {
   console.log('\nChecking Talawa-API connection....');
   let isConnected = false;
   await fetch(url)
-    .then(() => {
-      isConnected = true;
-      console.log('\nConnection to Talawa-API successful! 🎉');
-    })
+    // .then(() => {
+    //   isConnected = true;
+    //   console.log('\nConnection to Talawa-API successful! 🎉');
+    // })
     .catch(() => {
       // console.log(
       //   '\nTalawa-API service is unavailable. Is it running? Check your network connectivity too.'
