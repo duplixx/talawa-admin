8e91c189841dfbc3d1592c6d84f04f4753727857
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2024-02-03 17:02:10 +0530

    Update authorized-changes-detection.sh

--- .github/workflows/authorized-changes-detection.sh
+++ .github/workflows/authorized-changes-detection.sh
@@ -1,38 +1,37 @@
-##############################################################################
-##############################################################################
-#
-# NOTE!
-#
-# Please read the README.md file in this directory that defines what should 
-# be placed in this file
-#
-##############################################################################
-##############################################################################
+#!/bin/bash
 
-name: Checking workflow files
-on:
-  pull_request:
-    paths:
-      - '.github/**'   
-      - 'env.example'
-      - '.husky/**'
-      - 'scripts/**'
-      - 'package.json'
-      - 'tsconfig.json'
-      - '.gitignore'
-      - '.eslintrc.json'
-      - '.eslintignore '
-      - 'vite.config.ts'
-      - 'docker-compose.yaml'
-      - 'Dockerfile'      
-      - 'CODEOWNERS'
-      - 'LICENSE'
-      - 'setup.ts'
-      
-jobs:
-  Checking-for-unauthorized-file-changes:
-    name: Checking for unauthorized file changes
-    runs-on: ubuntu-latest
-    steps:
-      - name: Unauthorized file modification in PR
-        run: exit 1
+echo "List of unauthorized files changed:"
+# Add your list of sensitive files and directories here
+SENSITIVE_FILES=(
+  '.github/'
+  'env.example'
+  '.husky/'
+  'scripts/'
+  'package.json'
+  'tsconfig.json'
+  '.gitignore'
+  '.eslintrc.json'
+  '.eslintignore'
+  'vite.config.ts'
+  'docker-compose.yaml'
+  'Dockerfile'
+  'CODEOWNERS'
+  'LICENSE'
+  'setup.ts'
+)
+
+UNAUTHORIZED_CHANGES=false
+
+for file in $(git diff --name-only ${{ github.event.before || "HEAD^" }}); do
+  for sensitive_file in "${SENSITIVE_FILES[@]}"; do
+    if [[ "$file" == "$sensitive_file"* ]]; then
+      echo " - $file"
+      UNAUTHORIZED_CHANGES=true
+    fi
+  done
+done
+
+if [ "$UNAUTHORIZED_CHANGES" == true ]; then
+  echo "Unauthorized changes detected. Please review the list of modified files above."
+  exit 1
+fi
